a0e2e0275988a8d5a224776cada2a87130e34d4c
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2017-11-27 13:22:53 -0600

    Update 13.1.2.py

--- 13.1.2.py
+++ 13.1.2.py
@@ -5,9 +5,9 @@ import RoboPiLib as RPL
 RPL.servoWrite(0,1000)
 start = time.time()
 
-elapsed == (time.time() - start)
+elapsed = (time.time() - start)
 for integer in time.time():
-    if elapsed = 3:
+    if elapsed == 3:
         import RoboPiLib as RPL
         import setup
         RPL.servoWrite(0,0)
